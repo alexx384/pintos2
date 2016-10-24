82ce27c2b9c4a1be405cfa0d1dfb0da50fc1f9db
foxik384 <alexx384@yandex.ru>
2016-10-24 17:01:45 +0300

    First condition

---
+++ src/tests/threads/narrow-bridge.c
@@ -0,0 +1,27 @@
+
+/* File for 'narrow_bridge' task implementation.  
+   Only output required. 
+   SPbSTU, IBKS, 2016 */
+
+#include <stdio.h>
+#include "tests/threads/tests.h"
+#include "threads/thread.h"
+#include "threads/synch.h"
+#include "narrow-bridge.h"
+
+// Called before test
+void narrow_bridge_init(unsigned int num_vehicles_left, unsigned int num_vehicles_right,
+        unsigned int num_emergency_left, unsigned int num_emergency_right)
+{
+	// Not implemented
+}
+
+void arrive_bridge(enum car_priority prio, enum car_direction dir)
+{
+	// Not implemented
+}
+
+void exit_bridge(enum car_priority prio, enum car_direction dir)
+{
+	// Not implemented
+}
